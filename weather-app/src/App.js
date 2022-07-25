b3996718c4bad5fa0aaf7a5a18dd7105a3e9f3c9
munenebrian <njerubrianmunene@gmail.com>
2022-07-26 01:31:04 +0300

    created the forth component and worked on it!

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,6 +3,7 @@ import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButton from './components/TopButton';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
 
 function App() {
   
@@ -10,8 +11,9 @@ function App() {
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  from-cyan-700 to-blue-700 h-fit shodow-xl shadow-gray-400">
       <TopButton/>
       <Inputs/>
-      
+
       <TimeAndLocation/>
+      <TemperatureAndDetails/>
     </div>
   );
 }
