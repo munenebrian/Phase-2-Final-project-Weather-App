746e8e91689b3aa0ed253df67a3fe9a9ef9ab6f8
munenebrian <njerubrianmunene@gmail.com>
2022-07-25 13:56:16 +0300

    getting rid of unneccessary files

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Search from './components/Search';
 function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  }
   return (
-    <div className="App">
-      <Search/>
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange}/>
     </div>
   );
 }
