e838eb62bb36a15c689e21a9502d2ee5e682036d
munenebrian <njerubrianmunene@gmail.com>
2022-07-26 02:05:59 +0300

    added on my fith component!

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,6 +4,7 @@ import TopButton from './components/TopButton';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
 
 function App() {
   
@@ -14,6 +15,9 @@ function App() {
 
       <TimeAndLocation/>
       <TemperatureAndDetails/>
+
+      <Forecast title="hourly forecast"/>
+      <Forecast title="daily forecast"/>
     </div>
   );
 }
