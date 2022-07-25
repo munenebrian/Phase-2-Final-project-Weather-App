c8674e1c716a82f8e25f056207a1a24c5da7e422
munenebrian <njerubrianmunene@gmail.com>
2022-07-25 21:06:38 +0300

    added the first component!

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButton from './components/TopButton';
 
 function App() {
   
   return (
-    <div className="App">
-      <h2>hello world</h2>
+    <div className="mx-auot max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shodow-xl shadow-gray-400">
+      <TopButton/>
     </div>
   );
 }
