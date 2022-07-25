5f69add9cea3c9276ccc33867e7e176ba1a0df21
munenebrian <njerubrianmunene@gmail.com>
2022-07-25 21:21:43 +0300

    added the second component!

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButton from './components/TopButton';
+import Inputs from './components/Inputs';
 
 function App() {
   
   return (
     <div className="mx-auot max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shodow-xl shadow-gray-400">
       <TopButton/>
+      <Inputs/>
     </div>
   );
 }
---
+++ weather-app/src/components/Inputs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { UilAirplay, UilLocationPoint } from '@iconscout/react-unicons'
+
+function Inputs() {
+  return (
+    <div className="flex flex-row justify-center my-6">
+        <div className="flex flex-row w-3/4 items-center space-x-4">
+            <input type="text" className="text-xl font-light p-2 w-full shadow-xl"/>
+        </div>
+    </div>
+  )
+}
+
+export default Inputs
