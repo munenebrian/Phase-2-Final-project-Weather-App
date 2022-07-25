2414198a4445fe232fb3507def4f1b503d6eec1d
munenebrian <njerubrianmunene@gmail.com>
2022-07-25 22:00:07 +0300

    added the third component,and woked on some few details!

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButton from './components/TopButton';
 import Inputs from './components/Inputs';
+import TimeAndLocation from './components/TimeAndLocation';
 
 function App() {
   
@@ -9,6 +10,8 @@ function App() {
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  from-cyan-700 to-blue-700 h-fit shodow-xl shadow-gray-400">
       <TopButton/>
       <Inputs/>
+      
+      <TimeAndLocation/>
     </div>
   );
 }
