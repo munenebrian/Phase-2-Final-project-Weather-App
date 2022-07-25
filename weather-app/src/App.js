c3d609f28a23892f7288d31d5617d4512252d92b
munenebrian <njerubrianmunene@gmail.com>
2022-07-25 21:35:44 +0300

    added icons to the input bar!

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,7 +6,7 @@ import Inputs from './components/Inputs';
 function App() {
   
   return (
-    <div className="mx-auot max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shodow-xl shadow-gray-400">
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  from-cyan-700 to-blue-700 h-fit shodow-xl shadow-gray-400">
       <TopButton/>
       <Inputs/>
     </div>
--- weather-app/src/components/Inputs.js
+++ weather-app/src/components/Inputs.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { UilAirplay, UilLocationPoint } from '@iconscout/react-unicons'
+import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons'
 
 function Inputs() {
   return (
     <div className="flex flex-row justify-center my-6">
         <div className="flex flex-row w-3/4 items-center space-x-4">
-            <input type="text" className="text-xl font-light p-2 w-full shadow-xl"/>
+            <input 
+            type="text"
+            placeholder="Search for city...."
+            className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
+            />
+            <UilSearch size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"/>
+            <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"/>
         </div>
     </div>
   )
