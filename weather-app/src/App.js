7c6827821b8c7ac286e3ccff6373a18d17f49df0
munenebrian <njerubrianmunene@gmail.com>
2022-07-26 13:26:42 +0300

    fetching data!

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -10,18 +10,21 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [query, setQuery] = useState({q: 'berlin'})
+  const [query, setQuery] = useState({q: "berlin"})
   const [units, setUnits] = useState('metric')
   const [weather, setWeather] = useState(null)
 
   useEffect(() => {
     const fetchWeather = async () => {
-      const data = await getFormattedWeatherData({q: 'london'});
-      console.log(data);
-    };
+     await getFormattedWeatherData({...query, units})
+     .then(data => {
+      setWeather(data);
+     });
     
+    };
+
     fetchWeather();
-  }, [])
+  }, [query, units]);
   
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  from-cyan-700 to-blue-700 h-fit shodow-xl shadow-gray-400">
